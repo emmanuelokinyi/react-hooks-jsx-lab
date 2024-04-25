64f5247bcd6b5e9d7c908e6821876e5c772d3b59
Emmanuel <emmanuelokinyi799@gmail.com>
2024-04-26 02:38:21 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Hello,there. I am Emmanuel Okinyi,a dedicated software developer equipped with up to date skills and ready to solve your tech problems.
+    </p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
